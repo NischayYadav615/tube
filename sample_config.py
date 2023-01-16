02ae0f3eeb4939b6d466add11e57de534a0a9493
NischayYadav615 <112478816+NischayYadav615@users.noreply.github.com>
2023-01-16 08:22:18 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -17,25 +17,27 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 class Config(object):
     # Get a bot token from botfather
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5884076474:AAECzb9MHCp03LrvI8YKJ6YuX879gp7lBBY")
 
     # Get from my.telegram.org
-    APP_ID = int(os.environ.get("APP_ID", ""))
+    APP_ID = int(os.environ.get("APP_ID", "19911978"))
 
     # Get from my.telegram.org
-    API_HASH = os.environ.get("API_HASH", "")
+    API_HASH = os.environ.get("API_HASH", "e3f5848d4c384af9e6f1f52ca84c19c7")
 
     # Authorized user ids to use this bot
-    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
+    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "fase").split())
 
     # superusers to broadcast messages & fetch subscribers count
-    SUDO_USERS = set(int(x) for x in os.environ.get("SUDO_USERS", "").split())
+    SUDO_USERS = set(int(x) for x in os.environ.get("SUDO_USERS", "5826950002
+
+").split())
 
     # Database URI
-    DB_URI = os.environ.get("DATABASE_URL", "")
+    DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Nischaybot:Nischaybot@cluster0.thf9kzz.mongodb.net/?retryWrites=true&w=majority")
 
     # Force subscribe channel / group id starting with -100
-    FORCE_SUB_CHAT = os.environ.get("FORCE_SUB_CHAT", "")
+    FORCE_SUB_CHAT = os.environ.get("FORCE_SUB_CHAT", "-1001881722859")
 
     # Telegram maximum file upload size
     TG_MAX_FILE_SIZE = 2097152000
